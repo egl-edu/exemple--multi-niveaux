ab0f79667672780bdc11b6d5af8b5ab640c0d36f
Enric Granzotto Llagostera <egl-edu@proton.me>
2025-10-23 11:20:23 -0400

    Deux niveaux avec environnements différents

--- projet_godot/scripts/main.gd
+++ projet_godot/scripts/main.gd
@@ -2,4 +2,4 @@ extends Node
 
 func changer_scene(nouvelle_scene:PackedScene):
 	print("changer scene")
-	get_tree().change_scene_to_packed(nouvelle_scene)
+	get_tree().call_deferred("change_scene_to_packed", nouvelle_scene)
