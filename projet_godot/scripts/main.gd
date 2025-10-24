900d940dd25118e9ca1abe2139e111cde01f4566
egl-edu <egl-edu@proton.me>
2025-10-23 21:09:21 -0400

    HUD et menu de pause pour les 2 niveaux

--- projet_godot/scripts/main.gd
+++ projet_godot/scripts/main.gd
@@ -1,5 +1,13 @@
 extends Node
 
+@export var monnaies = 0
+signal monnaie_collectee
+
+
+func augmenter_monnaies():
+	monnaies += 1
+	monnaie_collectee.emit(monnaies)
+
 func changer_scene(nouvelle_scene:PackedScene):
 	print("changer scene")
 	get_tree().call_deferred("change_scene_to_packed", nouvelle_scene)
